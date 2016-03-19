dd9e22d4603cde2d73646a518b3013abfc0eb43e
York Chen <qgmzwrym@gmail.com>
2016-03-20 07:22:00 +0800

    add js file for home page.

---
+++ app/homePage.js
@@ -0,0 +1,7 @@
+/**
+ * Created by jasperchiu on 3/19/16.
+ */
+//这个文件主要用来操作dom元素
+$(function () {
+    ko.applyBindings(new window.BPMN.ViewModels.HomeViewModel());
+})
---
+++ app/viewmodels/homeViewModel.js
@@ -0,0 +1,9 @@
+(function (BPMN, $, ko) {
+
+   BPMN.ViewModels = BPMN.ViewModels || {};
+   BPMN.ViewModels.HomeViewModel = function () {
+      var self = this;
+
+   };
+
+})(window.BPMN = window.BPMN || {}, jQuery, ko)
