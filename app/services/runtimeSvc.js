6a62af0a1c4f080553826a0144c7327ee0525839
chunchill <chunchill@163.com>
2016-03-24 00:07:12 +0800

    add the post process instance services

--- app/services/runtimeSvc.js
+++ app/services/runtimeSvc.js
@@ -3,24 +3,30 @@
  */
 (function (BPMS, $, amplify) {
 
-   BPMS.Services = BPMS.Services || {};
-   BPMS.Services.RuntimeSvc = (function () {
-      var serviceUrl = BPMS.config.serviceUrl;
-      var getTasks = function (options) {
-         return BPMS.Services.Utils.restfulRequest('getTasks', 'runtime/tasks', options);
-      };
-      var postTasks = function (id,options) {
-         return BPMS.Services.Utils.restfulPost('postTasks', 'runtime/tasks/' + id, options);
-      };
-      var getProcessInstances = function (options) {
-         return BPMS.Services.Utils.restfulRequest('getProcessInstances', 'runtime/process-instances', options);
-      };
-      return {
-         getTasks: getTasks,
-         postTasks: postTasks,
-         getProcessInstances: getProcessInstances
-      };
-   })();
+    BPMS.Services = BPMS.Services || {};
+    BPMS.Services.RuntimeSvc = (function () {
+        var serviceUrl = BPMS.config.serviceUrl, getTasks = function (options) {
+            return BPMS.Services.Utils.restfulRequest('getTasks', 'runtime/tasks', options);
+        }, postTasks = function (id, options) {
+            return BPMS.Services.Utils.restfulPost('postTasks', 'runtime/tasks/' + id, options);
+        }, getProcessInstances = function (options) {
+            return BPMS.Services.Utils.restfulRequest('getProcessInstances', 'runtime/process-instances', options);
+        }, postProcessInstance = function (options) {
+            return BPMS.Services.Utils.restfulPost('postProcessInstance', 'runtime/process-instances', options);
+        }, postProcessInstanceIndentityLinks = function (id, options) {
+            return BPMS.Services.Utils.restfulPost('postProcessInstanceIndentityLinks', 'runtime/process-instances/' + id + '/identitylinks', options);
+        }, postProcessInstanceVariables = function (id, options) {
+            return BPMS.Services.Utils.restfulPost('postProcessInstanceVariables', 'runtime/process-instances/' + id + '/variables', options);
+        };
+        return {
+            getTasks: getTasks,
+            getProcessInstances: getProcessInstances,
+            postTasks: postTasks,
+            postProcessInstance: postProcessInstance,
+            postProcessInstanceIndentityLinks: postProcessInstanceIndentityLinks,
+            postProcessInstanceVariables: postProcessInstanceVariables
+        };
+    })();
 
 
 })(window.BPMS = window.BPMS || {}, jQuery, amplify)
