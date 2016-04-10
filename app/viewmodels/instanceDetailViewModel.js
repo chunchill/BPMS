97a4880bae0d5b39fed1ed50da07c05c755e7401
chenyork <qgmzwrym@gmail.com>
2016-04-09 22:34:54 -0700

    fix bugs

--- app/viewmodels/instanceDetailViewModel.js
+++ app/viewmodels/instanceDetailViewModel.js
@@ -1,103 +1,107 @@
 (function (BPMS, $, ko) {
 
-   BPMS.ViewModels = BPMS.ViewModels || {};
-   //登录页面viewmodel
-   BPMS.ViewModels.InstanceDetailViewModel = function () {
-      var self = this;
-      self.userId = localStorage.getItem("bpms_userId");
-      self.processInstanceId = BPMS.Services.Utils.getUrlParam(window.location.href, "processInstanceId");
-      self.processDefinitionId = BPMS.Services.Utils.getUrlParam(window.location.href, "processDefinitionId");
-      var data = localStorage.getItem("instance");
-      data = data && JSON.parse(data);
-      if (!self.processInstanceId || !self.processDefinitionId
-         || !data || !data.id || !data.processDefinitionId
-         || data.id != self.processInstanceId || data.processDefinitionId != self.processDefinitionId)
-         window.location.href = "home.html";
+    BPMS.ViewModels = BPMS.ViewModels || {};
+    //登录页面viewmodel
+    BPMS.ViewModels.InstanceDetailViewModel = function () {
+        var self = this;
+        self.userId = localStorage.getItem("bpms_userId");
+        self.processInstanceId = BPMS.Services.Utils.getUrlParam(window.location.href, "processInstanceId");
+        self.processDefinitionId = BPMS.Services.Utils.getUrlParam(window.location.href, "processDefinitionId");
+        var data = localStorage.getItem("instance");
+        data = data && JSON.parse(data);
+        if (!self.processInstanceId || !self.processDefinitionId
+           || !data || !data.id || !data.processDefinitionId
+           || data.id != self.processInstanceId || data.processDefinitionId != self.processDefinitionId)
+            window.location.href = "home.html";
 
-      self.data = data;
+        self.data = data;
 
-      self.flows = ko.observableArray();
-      self.tasks = ko.observableArray();
+        self.flows = ko.observableArray();
+        self.tasks = ko.observableArray();
 
-      self.flow = {
-         "decision": ko.observable(),
-         "assignee": ko.observable(),
-         "date": ko.observable(),
-         "time": ko.observable(),
-         "comment": ko.observable()
-      };
+        self.flow = {
+            "decision": ko.observable(),
+            "assignee": ko.observable(),
+            "date": ko.observable(),
+            "time": ko.observable(),
+            "comment": ko.observable()
+        };
 
-      self.formatDate = function (date) {
-         if (!date) return "";
-         return moment(date).format("YYYY-MM-DD");
-      };
+        self.formatDate = function (date) {
+            if (!date) return "";
+            return moment(date).format("YYYY-MM-DD");
+        };
 
-      self.formatTime = function (date) {
-         if (!date) return "";
-         return moment(date).format("HH:mm");
-      };
-      self.formatDateTime = function (dateTime) {
-         if (!dateTime) return "";
-         return moment(dateTime).format("YYYY-MM-DD HH:mm");
-      };
-      self.translateValue = function (value) {
-         if (value == null || typeof (value) == "undefined")
-            return "";
-         if (value === "true")
-            return "同意";
-         if (value === "false")
-            return "拒绝";
+        self.formatTime = function (date) {
+            if (!date) return "";
+            return moment(date).format("HH:mm");
+        };
+        self.formatDateTime = function (dateTime) {
+            if (!dateTime) return "";
+            return moment(dateTime).format("YYYY-MM-DD HH:mm");
+        };
+        self.translateValue = function (value) {
+            if (value == null || typeof (value) == "undefined")
+                return "";
+            if (value === "true")
+                return "同意";
+            if (value === "false")
+                return "拒绝";
 
-         if (typeof (value) == "boolean") return value ? "同意" : "拒绝";
-         return value;
-      };
+            if (typeof (value) == "boolean") return value ? "同意" : "拒绝";
+            return value;
+        };
 
-      self.selectFlow = function ($data) {
-         self.flow.decision($data.decision || "");
-         self.flow.assignee($data.assignee || "");
-         self.flow.date(self.formatDate($data.endTime));
-         self.flow.time(self.formatTime($data.endTime));
-         self.flow.comment($data.comment || "");
-      };
-      self.init = function () {
+        self.selectFlow = function ($data) {
+            self.flow.decision($data.decision || "");
+            self.flow.assignee($data.assignee || "");
+            self.flow.date(self.formatDate($data.endTime));
+            self.flow.time(self.formatTime($data.endTime));
+            self.flow.comment($data.comment || "");
+        };
+        self.init = function () {
 
-         self.flows.removeAll();
-         self.tasks.removeAll();
+            self.flows.removeAll();
+            self.tasks.removeAll();
 
-         BPMS.Services.FormSvc.getFormData({ "processDefinitionId": self.processDefinitionId }).then(
-          function (result) {
-             if (result && result && result.formProperties)
-                result.formProperties.forEach(
-                   function (item) {
-                      // if (!item.writable)
-                      self.tasks.push(item);
-                   }
-                );
-          });
-
-
-         BPMS.Services.HistoryInstancesSvc.getActivityInstances({ "processInstanceId": self.processInstanceId })
-            .then(function (result) {
-               result.data.forEach(
-                  function (item) {
-                     self.flows.push(item);
-                  });
-            });
+            BPMS.Services.FormSvc.getFormData({ "processDefinitionId": self.processDefinitionId }).then(
+             function (result) {
+                 if (result && result && result.formProperties)
+                     result.formProperties.forEach(
+                        function (item) {
+                            // if (!item.writable)
+                            self.tasks.push(item);
+                        }
+                     );
+             });
 
 
+            BPMS.Services.HistoryInstancesSvc.getActivityInstances({
+                "processInstanceId": self.processInstanceId,
+                "sort": "startTime",
+                "order": "asc"
+            })
+              .then(function (result) {
+                  result.data.forEach(
+                     function (item) {
+                         self.flows.push(item);
+                     });
+              });
 
-         BPMS.Services.HistoryInstancesSvc.getVariableInstances({ "processInstanceId": self.processInstanceId })
-            .then(function (result) {
-               result.data.forEach(
-                  function (item) {
-
-                  }
-               );
-            });
 
 
-      }
-   }
+            BPMS.Services.HistoryInstancesSvc.getVariableInstances({ "processInstanceId": self.processInstanceId })
+               .then(function (result) {
+                   result.data.forEach(
+                      function (item) {
+
+                      }
+                   );
+               });
+
+
+        }
+    }
 }
 
 )(window.BPMS = window.BPMS || {}, jQuery, ko)
--- app/viewmodels/taskActionViewModel.js
+++ app/viewmodels/taskActionViewModel.js
@@ -1,194 +1,198 @@
 (function (BPMS, $, ko, moment) {
 
-   BPMS.ViewModels = BPMS.ViewModels || {};
-   //登录页面viewmodel
-   BPMS.ViewModels.TaskActionViewModel = function () {
-      var writableFields = [];
-      var keyValue = {};
-      var self = this;
+    BPMS.ViewModels = BPMS.ViewModels || {};
+    //登录页面viewmodel
+    BPMS.ViewModels.TaskActionViewModel = function () {
+        var writableFields = [];
+        var keyValue = {};
+        var self = this;
 
-      self.userId = localStorage.getItem("bpms_userId");
-      self.taskId = BPMS.Services.Utils.getUrlParam(window.location.href, "taskId");
-      self.processInstanceId = BPMS.Services.Utils.getUrlParam(window.location.href, "processInstanceId");
-      self.name = ko.observable();
-      self.mappings = [];
-      self.processDefinitionId = "";
-      self.description = ko.observable();
-      self.started = ko.observable(false);
-      self.showStart = ko.observable(true);
-      self.submitted = ko.observable(false);
-      self.tasks = ko.observableArray();
-      self.forms = ko.observableArray();
-      self.flows = ko.observableArray();
-      self.formatDateTime = function (dateTime) {
-         if (!dateTime) return "";
-         return moment(dateTime).format("YYYY-MM-DD HH:mm");
-      };
-      self.formatDate = function (dateTime) {
-         if (!dateTime) return "";
-         return moment(dateTime).format("YYYY-MM-DD");
-      };
-      self.formatTime = function (dateTime) {
-         if (!dateTime) return "";
-         return moment(dateTime).format("HH:mm");
-      };
-      self.bindFormData = function () {
-         writableFields.forEach(
-         function (item) {
+        self.userId = localStorage.getItem("bpms_userId");
+        self.taskId = BPMS.Services.Utils.getUrlParam(window.location.href, "taskId");
+        self.processInstanceId = BPMS.Services.Utils.getUrlParam(window.location.href, "processInstanceId");
+        self.name = ko.observable();
+        self.mappings = [];
+        self.processDefinitionId = "";
+        self.description = ko.observable();
+        self.started = ko.observable(false);
+        self.showStart = ko.observable(true);
+        self.submitted = ko.observable(false);
+        self.tasks = ko.observableArray();
+        self.forms = ko.observableArray();
+        self.flows = ko.observableArray();
+        self.formatDateTime = function (dateTime) {
+            if (!dateTime) return "";
+            return moment(dateTime).format("YYYY-MM-DD HH:mm");
+        };
+        self.formatDate = function (dateTime) {
+            if (!dateTime) return "";
+            return moment(dateTime).format("YYYY-MM-DD");
+        };
+        self.formatTime = function (dateTime) {
+            if (!dateTime) return "";
+            return moment(dateTime).format("HH:mm");
+        };
+        self.bindFormData = function () {
+            writableFields.forEach(
+            function (item) {
 
-            var formItem = {
-               type: item.type,
-               value: ko.observable(item.value),
-               option: { name: item.name, id: item.id },
-               enumValues: item.enumValues
-            };
-            if (formItem.type == "text") {
-               formItem.option.data_clear_btn = true;
-            }
-            //jqm has a bug that id should be same as name for checkbox
-            if (formItem.type == "bool" || formItem.type == "boolean") {
-               keyValue[formItem.option.id] = formItem.option.name;
-               formItem.option.id = formItem.option.name;
-            }
+                var formItem = {
+                    type: item.type,
+                    value: ko.observable(item.value),
+                    option: { name: item.name, id: item.id },
+                    enumValues: item.enumValues
+                };
+                if (formItem.type == "text") {
+                    formItem.option.data_clear_btn = true;
+                }
+                //jqm has a bug that id should be same as name for checkbox
+                if (formItem.type == "bool" || formItem.type == "boolean") {
+                    keyValue[formItem.option.id] = formItem.option.name;
+                    formItem.option.id = formItem.option.name;
+                }
 
-            self.forms.push(formItem);
-         });
+                self.forms.push(formItem);
+            });
 
-      };
-      self.start = function () {
-         if (self.started())
-            return;
-         BPMS.Services.RuntimeSvc.postTasks(self.taskId, {
-            "action": "claim",
-            "assignee": self.userId
-         }).then(function () {
-             self.started(true);
-             location.hash = '#formArea';
-         }, function () {
-            $("#popupMessage").popup("open");
-         });
-      };
-
-      //[{ "name": "部门经理审批意见", "value": "同意" }]
-      self.submit = function () {
-         if (self.submitted())
-            return;
-         var values = [];
-         self.forms().forEach(
-         function (item) {
-            var id = item.option.id;
-            if (item.type == "bool" || item.type == "boolean") {
-               for (var key in keyValue) {
-                  if (id == keyValue[key]) {
-                     id = key;
-                  }
-               }
-            }
-            var value = item.value();
-            values.push({ "name": id, "value": value });
-         });
-         BPMS.Services.RuntimeSvc.postTasks(self.taskId, {
-            "action": "complete",
-            "variables": values
-         })
-            .then(function () {
-               self.submitted(true);
-               $("#popupsubmit").popup("open");
+        };
+        self.start = function () {
+            if (self.started())
+                return;
+            BPMS.Services.RuntimeSvc.postTasks(self.taskId, {
+                "action": "claim",
+                "assignee": self.userId
+            }).then(function () {
+                self.started(true);
+                location.hash = '#formArea';
             }, function () {
-               self.submitted(true);
-               $("#popupMessage").popup("open");
+                $("#popupMessage").popup("open");
             });
-      };
-      self.finish = function () {
-         window.location.href = "worklist.html";
-      };
+        };
 
-      self.flow = {
-         "decision": ko.observable(),
-         "assignee": ko.observable(),
-         "date": ko.observable(),
-         "time": ko.observable(),
-         "comment": ko.observable()
-      };
+        //[{ "name": "部门经理审批意见", "value": "同意" }]
+        self.submit = function () {
+            if (self.submitted())
+                return;
+            var values = [];
+            self.forms().forEach(
+            function (item) {
+                var id = item.option.id;
+                if (item.type == "bool" || item.type == "boolean") {
+                    for (var key in keyValue) {
+                        if (id == keyValue[key]) {
+                            id = key;
+                        }
+                    }
+                }
+                var value = item.value();
+                values.push({ "name": id, "value": value });
+            });
+            BPMS.Services.RuntimeSvc.postTasks(self.taskId, {
+                "action": "complete",
+                "variables": values
+            })
+               .then(function () {
+                   self.submitted(true);
+                   $("#popupsubmit").popup("open");
+               }, function () {
+                   self.submitted(true);
+                   $("#popupMessage").popup("open");
+               });
+        };
+        self.finish = function () {
+            window.location.href = "worklist.html";
+        };
 
-      self.selectFlow = function ($data) {
-         self.flow.decision($data.decision || "");
-         self.flow.assignee($data.assignee || "");
-         self.flow.date(self.formatDate($data.endTime));
-         self.flow.time(self.formatTime($data.endTime));
-         self.flow.comment($data.comment || "");
-      };
-      self.init = function () {
+        self.flow = {
+            "decision": ko.observable(),
+            "assignee": ko.observable(),
+            "date": ko.observable(),
+            "time": ko.observable(),
+            "comment": ko.observable()
+        };
 
-         self.tasks.removeAll();
-         self.flows.removeAll();
+        self.selectFlow = function ($data) {
+            self.flow.decision($data.decision || "");
+            self.flow.assignee($data.assignee || "");
+            self.flow.date(self.formatDate($data.endTime));
+            self.flow.time(self.formatTime($data.endTime));
+            self.flow.comment($data.comment || "");
+        };
+        self.init = function () {
 
-         var data = localStorage.getItem("task");
+            self.tasks.removeAll();
+            self.flows.removeAll();
 
-         data = data && JSON.parse(data);
-         if (data.assignee && data.assignee == self.userId) {
-            self.showStart(false);
-            self.started(true);
+            var data = localStorage.getItem("task");
 
-         }
-         if (!self.taskId || !self.processInstanceId
-            || !data || !data.id || !data.processInstanceId
-            || data.id != self.taskId || data.processInstanceId != self.processInstanceId) window.location.href = "home.html";
+            data = data && JSON.parse(data);
+            if (data.assignee && data.assignee == self.userId) {
+                self.showStart(false);
+                self.started(true);
 
-         self.name(data.name);
-         self.processDefinitionId = data.processDefinitionId;
-         self.translateValue = function (value) {
-            if (value == null || typeof (value) == "undefined")
-               return "";
-            if (value === "true")
-               return "同意";
-            if (value === "false")
-               return "拒绝";
-
-            if (typeof (value) == "boolean") return value ? "同意" : "拒绝";
-            return value;
-         };
-
-         self.description(data.description || "");
-
-         self.taskId = BPMS.Services.Utils.getUrlParam(window.location.href, "taskId");
-         BPMS.Services.FormSvc.getFormData({ /*"processDefinitionId": self.processDefinitionId */"taskId": self.taskId }).then(
-            function (result) {
-               if (result && result && result.formProperties)
-                  result.formProperties.forEach(
-                     function (item) {
-                        if (!item.writable)
-                           self.tasks.push(item);
-                        else
-                           writableFields.push(item);
-
-                     }
-                  );
-               self.bindFormData();
             }
-         );
+            if (!self.taskId || !self.processInstanceId
+               || !data || !data.id || !data.processInstanceId
+               || data.id != self.taskId || data.processInstanceId != self.processInstanceId) window.location.href = "home.html";
+
+            self.name(data.name);
+            self.processDefinitionId = data.processDefinitionId;
+            self.translateValue = function (value) {
+                if (value == null || typeof (value) == "undefined")
+                    return "";
+                if (value === "true")
+                    return "同意";
+                if (value === "false")
+                    return "拒绝";
+
+                if (typeof (value) == "boolean") return value ? "同意" : "拒绝";
+                return value;
+            };
+
+            self.description(data.description || "");
+
+            self.taskId = BPMS.Services.Utils.getUrlParam(window.location.href, "taskId");
+            BPMS.Services.FormSvc.getFormData({ /*"processDefinitionId": self.processDefinitionId */"taskId": self.taskId }).then(
+               function (result) {
+                   if (result && result && result.formProperties)
+                       result.formProperties.forEach(
+                          function (item) {
+                              if (!item.writable)
+                                  self.tasks.push(item);
+                              else
+                                  writableFields.push(item);
+
+                          }
+                       );
+                   self.bindFormData();
+               }
+            );
 
 
-         BPMS.Services.HistoryInstancesSvc.getVariableInstances({ "processInstanceId": self.processInstanceId })
-            .then(function (result) {
-               result.data.forEach(
-                  function (item) {
+            BPMS.Services.HistoryInstancesSvc.getVariableInstances({ "processInstanceId": self.processInstanceId })
+               .then(function (result) {
+                   result.data.forEach(
+                      function (item) {
 
-                  }
-               );
-            });
+                      }
+                   );
+               });
 
 
 
-         BPMS.Services.HistoryInstancesSvc.getActivityInstances({ "processInstanceId": self.processInstanceId })
-            .then(function (result) {
-               result.data.forEach(
-                  function (item) {
-                     self.flows.push(item);
-                  });
-            });
+            BPMS.Services.HistoryInstancesSvc.getActivityInstances({
+                "processInstanceId": self.processInstanceId,
+                "sort": "startTime",
+                "order": "asc"
+            })
+              .then(function (result) {
+                  result.data.forEach(
+                     function (item) {
+                         self.flows.push(item);
+                     });
+              });
 
-         //form/form-data 动态表单的中文描述
-      };
-   };
+            //form/form-data 动态表单的中文描述
+        };
+    };
 })(window.BPMS = window.BPMS || {}, jQuery, ko, moment);
